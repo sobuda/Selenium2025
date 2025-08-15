ad43613d2f90b9bb0d5dfebb7eb2cc994d3696a6
Sobuda <sowmyadibdalli@gmail.com>
2025-08-15 14:39:21 +0100

    Added Chromedriver

--- src/main/java/SeleniumSessions/XPathLearning.java
+++ src/main/java/SeleniumSessions/XPathLearning.java
@@ -1,9 +1,14 @@
 package SeleniumSessions;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class XPathLearning {
 
 	public static void main(String[] args) {
 
+		WebDriver driver = new ChromeDriver();
+		
 	}
 
 }
